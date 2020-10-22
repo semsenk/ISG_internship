2114790bd2563f05e0076c788bf6b29b81b27cf4
Semen Senkivskyi <semkosenk@gmail.com>
2020-10-22 14:03:24 +0300

    Update AnnotationDemo.java

--- annotationTask/AnnotationDemo.java
+++ annotationTask/AnnotationDemo.java
@@ -6,13 +6,11 @@ public class AnnotationDemo {
 
     public static void main(String[] args) {
         MacBook macBook = new MacBook("pro", 15);
-
-//        Class getClass = macBook.getClass();
-//        Annotation getAnnot = getClass.getAnnotation(Laptop.class);
-        System.out.printf("MacBook %s%n",
-                MacBook
+        System.out.println("MacBook " +
+                macBook
                         .getClass()
                         .getAnnotation(Laptop.class)
-                        .os());
+                        .os()
+        );
     }
 }
